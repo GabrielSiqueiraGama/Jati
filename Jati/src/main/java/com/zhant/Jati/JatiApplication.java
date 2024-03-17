6fee6e58e806649938db6eb3cec81d66e5d18990
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-03-17 15:59:02 -0300

    feat: Configurações iniciais

---
+++ Jati/src/main/java/com/zhant/Jati/JatiApplication.java
@@ -0,0 +1,13 @@
+package com.zhant.Jati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JatiApplication.class, args);
+	}
+
+}
---
+++ Jati/src/test/java/com/zhant/Jati/JatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhant.Jati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

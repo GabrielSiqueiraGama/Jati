03ebd4c607a768a5ee12825340c6045b7b019453
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-03-22 14:17:58 -0300

    Criando entidade usuario

---
+++ Jati/src/main/java/br/com/zhant/Jati/domain/usuario/Usuario.java
@@ -0,0 +1,29 @@
+package br.com.zhant.Jati.domain.usuario;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "tb_usuario")
+@Getter//Cria os getters
+@NoArgsConstructor// Cria um construtor sem argumentos
+@AllArgsConstructor//Cria um construtor com todos os argumentos
+@EqualsAndHashCode(of = "id")//Procura apelas pelo id
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+}
